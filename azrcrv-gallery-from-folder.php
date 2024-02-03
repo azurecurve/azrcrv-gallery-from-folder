d6961ed3b148bc412615016c0afc444429ef9337
azurecurve <github@azurecurve.co.uk>
2024-02-03 14:17:12 +0000

    Release v2.1.4

--- azrcrv-gallery-from-folder.php
+++ azrcrv-gallery-from-folder.php
@@ -1,14 +1,18 @@
 <?php
 /**
  * ------------------------------------------------------------------------------
- * Plugin Name: Gallery From Folder
- * Description: Gallery for all images in folder.
- * Version: 2.1.3
- * Author: azurecurve
- * Author URI: https://development.azurecurve.co.uk/classicpress-plugins/
- * Plugin URI: https://development.azurecurve.co.uk/classicpress-plugins/azrcrv-gallery-from-folder/
- * Text Domain: gallery-from-folder
- * Domain Path: /languages
+ * Plugin Name:		Gallery From Folder
+ * Description:		Gallery for all images in folder.
+ * Version:			2.1.4
+ * Requires CP:		1.0
+ * Author:			azurecurve
+ * Author URI:		https://development.azurecurve.co.uk/classicpress-plugins/
+ * Plugin URI:		https://development.azurecurve.co.uk/classicpress-plugins/azrcrv-gallery-from-folder/
+ * Donate link:		https://development.azurecurve.co.uk/support-development/
+ * Text Domain:		gallery-from-folder
+ * Domain Path:		/languages
+ * License:			GPLv2 or later
+ * License URI:		http://www.gnu.org/licenses/gpl-2.0.html
  * ------------------------------------------------------------------------------
  * This is free software released under the terms of the General Public License,
  * version 2, or later. It is distributed WITHOUT ANY WARRANTY; without even the
